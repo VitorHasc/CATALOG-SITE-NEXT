29e07d1dd8654c317b75b191c8f553e8f387eda8
Micro11 <vitorlegalhugi@gmail.com>
2024-10-11 15:01:47 -0300

    Teste da pagina pages/landpage, funcionando com rota

---
+++ src/app/components/Hero.js
@@ -0,0 +1,29 @@
+// Hero.js
+import React from 'react';
+
+
+const Hero = () => {
+  return (
+    <section 
+      className="hero h-screen bg-cover bg-center relative" 
+    >
+      <div className="overlay absolute inset-0 bg-black bg-opacity-50"></div>
+      <div className="container mx-auto relative z-10 flex flex-col items-center justify-center text-center h-full text-white">
+        <h1 className="text-5xl font-bold">Encontre o seu Lar perfeito.</h1>
+        <div className="mt-5 relative w-96">
+          <input 
+            type="text" 
+            placeholder="Tipo de imóvel..." 
+            className="w-full py-3 px-5 rounded-full focus:outline-none"
+          />
+          <button 
+            className="">
+            <i className="fas fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
