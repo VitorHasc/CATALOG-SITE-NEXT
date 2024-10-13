b1bb8740e5ff9c199cb6ac7b80c3c586e560f479
vitor hugo de abreu schell <vitorlegalhugi@gmail.com>
2024-10-13 15:09:41 -0300

    Teste

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -11,11 +11,13 @@ const Hero = () => {
       <div className="container mx-auto relative z-10 flex flex-col items-center justify-center text-center h-full text-white">
         <h1 className="text-5xl font-bold">Encontre o seu Lar perfeito.</h1>
         <div className="mt-5 relative w-96">
-          <input 
-            type="text" 
-            placeholder="Tipo de imóvel..." 
-            className="w-full py-3 px-5 rounded-full focus:outline-none"
-          />
+        <input 
+  type="text" 
+  placeholder="Tipo de imóvel..." 
+  className="w-full py-3 px-5 rounded-full focus:outline-none"
+  style={{ color: 'black' }}
+/>
+
           <button 
             className="">
             <i className="fas fa-search"></i>
